fc8b4ab10f79ae9414e5eb3d759af3fd924abbcf
ldc-369 <ldc369tpqt@gmail.com>
2025-07-01 17:41:30 +0700

    commit

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 import pickle
-import streamlit as st
 
 # load model
-model = load_model("./model/model.h5")
+model = load_model("./model/model.keras")
 
 with open('./model/X_scaler.pkl', 'rb') as f1:
    X_scaler = pickle.load(f1)
    
-with open('./model/y_scaler.pkl', 'rb') as f2:
-   y_scaler = pickle.load(f2)
+with open('./model/Y_scaler.pkl', 'rb') as f2:
+   Y_scaler = pickle.load(f2)
 
 with open('./model/ocp_encoder.pkl', 'rb') as f3:
     ocp_encoder = pickle.load(f3)
 
 def prepare_input(longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, ocean_proximity):
     # encode
-    ocp_encoded = ocp_encoder.transform(np.array([[ocean_proximity.lower()]])) # (1, 5)
+    ocp_encoded = ocp_encoder.transform(np.array([[ocean_proximity.lower()]])) # array(1, 5)
 
-    X_remain = np.array([[longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income]]) # (1, 8)
-    X_predict = np.concatenate((X_remain, ocp_encoded), axis=1)  # (1, 13)
+    X_remain = np.array([[longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income]]) # array(1, 8)
+    X_predict = np.concatenate((X_remain, ocp_encoded), axis=1)  # array(1, 13)
 
     # scale
     X_predict = X_scaler.transform(X_predict)
@@ -40,7 +39,7 @@ input = {
 }
 
 X_pred = prepare_input(**input)
-y_pred = model.predict(X_pred)
-y_pred = y_scaler.inverse_transform(y_pred)
+Y_pred = model.predict(X_pred)
+Y_pred = Y_scaler.inverse_transform(Y_pred)
 
-print(y_pred)
+print(Y_pred)
